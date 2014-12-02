0e5280a4df5b8ffecad0b405a813c579cedc11ae
Anna Jerofejeva <jerofejeva.anna@yahoo.com>
2014-12-02 22:29:11 +0000

    added keys

--- config.js
+++ config.js
@@ -2,8 +2,8 @@ exports.port = 8000;
 exports.upload_dir = './uploads';
 
 exports.s3 = {
-    key: process.env.key,
-    secret: process.env.secret,
+    key: process.env.aws_access_key_id,
+    secret: process.env.aws_secret_access_key,
     bucket: 'stackovertube'
 };
 
